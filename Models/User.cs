07e32ab3bac511e29847768cccc0c7cb6d75f387
lucasszmt <lucasszmt@gmail.com>
2020-08-19 11:16:53 -0300

    min char alteres

--- Models/User.cs
+++ Models/User.cs
@@ -9,7 +9,7 @@ namespace Shop.Models
 
         [Required(ErrorMessage = "Este campo é obrigatório")]
         [MaxLength(20, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
-        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        [MinLength(4, ErrorMessage = "Este campo deve conter entre 4 e 60 caracteres")]
         public string Username { get; set; }
 
         [Required(ErrorMessage = "Este campo é obrigatório")]
